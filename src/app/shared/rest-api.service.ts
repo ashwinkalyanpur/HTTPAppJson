77a0aa3e06d45bdcd07944aa28e2ffe9c1c72739
ashwinkalyanpur <akalyanpur1974@gmail.com>
2020-11-28 01:46:23 +0530

    File updated system error to be fixed

---
+++ src/app/shared/rest-api.service.ts
@@ -0,0 +1,88 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Employee } from '../shared/employee';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class RestApiService {
+  
+  // Define API
+  apiURL = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) { }
+
+  /*========================================
+    CRUD Methods for consuming RESTful API
+  =========================================*/
+
+  // Http Options
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }  
+
+  // HttpClient API get() method => Fetch vendor list
+  getEmployees(): Observable<Employee> {
+    return this.http.get<Employee>(this.apiURL + '/vendor')
+    .pipe(
+      retry(1),
+      catchError(this.handleError)
+    )
+  }
+
+  // HttpClient API get() method => Fetch employee
+  getEmployee(id): Observable<Employee> {
+    return this.http.get<Employee>(this.apiURL + '/vendor/' + id)
+    .pipe(
+      retry(1),
+      catchError(this.handleError)
+    )
+  }  
+
+  // HttpClient API post() method => Create employee
+  createEmployee(employee): Observable<Employee> {
+    return this.http.post<Employee>(this.apiURL + '/vendor', JSON.stringify(employee), this.httpOptions)
+    .pipe(
+      retry(1),
+      catchError(this.handleError)
+    )
+  }  
+
+  // HttpClient API put() method => Update employee
+  updateEmployee(id, employee): Observable<Employee> {
+    return this.http.put<Employee>(this.apiURL + '/vendor/' + id, JSON.stringify(employee), this.httpOptions)
+    .pipe(
+      retry(1),
+      catchError(this.handleError)
+    )
+  }
+
+  // HttpClient API delete() method => Delete employee
+  deleteEmployee(id){
+    return this.http.delete<Employee>(this.apiURL + '/vendor/' + id, this.httpOptions)
+    .pipe(
+      retry(1),
+      catchError(this.handleError)
+    )
+  }
+
+  // Error handling 
+  handleError(error) {
+     let errorMessage = '';
+     if(error.error instanceof ErrorEvent) {
+       // Get client-side error
+       errorMessage = error.error.message;
+     } else {
+       // Get server-side error
+       errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+     }
+     window.alert(errorMessage);
+     return throwError(errorMessage);
+  }
+
+}
